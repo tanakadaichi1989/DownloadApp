186d080bc6389c45b381f3d746e65364bd1956c3
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-03-18 10:08:29 +0900

    Initial Commit

---
+++ DownloadApp/DownloadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadAppApp.swift
+//  DownloadApp
+//
+//  Created by 田中大地 on 2022/03/18.
+//
+
+import SwiftUI
+
+@main
+struct DownloadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

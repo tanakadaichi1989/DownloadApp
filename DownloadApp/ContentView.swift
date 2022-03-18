5b2bcd34e3ac2b3ee48ffb73911273990b683ba5
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-03-18 11:58:33 +0900

    プログレスバーの更新処理を追加

--- DownloadApp/ContentView.swift
+++ DownloadApp/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress:Int = 0
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            ProgressView("\(progress)%",value: Float(progress),total: Float(100))
+                .progressViewStyle(.linear)
+                .padding()
+            Button(action: tapButton,label: {
+                Text("Start")
+            })
+            .buttonStyle(.borderedProminent)
+            Spacer()
+        }
+    }
+    
+    private func tapButton(){
+        DownloadManager.startDownload { progress in
+            self.progress = progress
+        }
     }
 }
 
--- DownloadApp/DownloadManager.swift
+++ DownloadApp/DownloadManager.swift
@@ -5,4 +5,23 @@
 //  Created by 田中大地 on 2022/03/18.
 //
 
+// https://qiita.com/Yaruki00/items/ff991c5f3c2963a14b3a
+
 import Foundation
+
+class DownloadManager {
+    static func startDownload(completion: @escaping (Int) -> Void){
+        print("start Download")
+        var progress = 0
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.0){
+        while(progress <= 100){
+            print(progress)
+            completion(progress)
+            progress += 1
+            sleep(1)
+        }
+        }
+    }
+}
+
